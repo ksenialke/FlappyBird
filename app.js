ea659a9ca636cb846c6c4478a2ee9bbaadb50f87
Ksenia Klamut <ksenia.klamut@gmail.com>
2019-03-26 23:03:24 +0100

    fixed

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(
-    "/",
-    express.static(__dirname)
-);
+app.use(express.static('static'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
